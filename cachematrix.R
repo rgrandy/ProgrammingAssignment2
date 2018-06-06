6bd8c093049a65ae8dcd55b467f070a6a3c44211
rgrandy <rcgrandy@gmail.com>
2018-06-06 05:54:30 -0500

    Update #2
    Update of Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program includes the functions makeCacheMatric and cacheSolve
+## makeCacheMatrix takes a matrix as input, obtaisn the inverse and saves
+## all 
 
-## Write a short comment describing this function
+## makeCacheMatrix computes the matrix inverse and returns a list
+## with the matrix and the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  # Initialize the inverse to NULL
   MyInv <- NULL
+  # set is saved outside of the environment of this function
   set <- function(y) {
     x <<- y
     MyInv <<- NULL
   }
+  # get is the original input
   get <- function() x
+
   setInverse <- function(inverse) MyInv <<- inverse #calculate the inverse
   getInverse <- function() MyInv
+  
+  #This function returns a list of all of these pieces
   list(set = set,
        get = get,
        setInverse = setInverse,
@@ -20,19 +28,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve looks to see if there is an inverse already for the matrix
+## and returns that matrix.  The second time it is called it gets the
+## cached inverse and is faster than the first
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  MyInv <- x$getInverse()
-  if (!is.null(MyInv)) {
-    message("getting cached data")
-    return(MyInv)
-  }
-  mat <- x$get()
-  MyInv <- solve(mat, ...)
-  x$setInverse(MyInv)
-  MyInv
-  
+    ## Return a matrix that is the inverse of 'x'
+    MyInv <- x$getInverse()
+    if (!is.null(MyInv)) {
+     message("getting cached data")
+     return(MyInv)
+   }
+   getthis <- x$get()
+    MyInv <- solve(getthis, ...)
+    x$setInverse(MyInv)
+   MyInv
 }
+
+## EXAMPLE CALL
+M <- matrix(c(1,4,5,7),2,2)
+M1 <- makeCacheMatrix(M)
+cacheSolve(M1)
+cacheSolve(M1)
 
